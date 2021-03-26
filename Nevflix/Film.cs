a5b193774c743af192690a1fa3517a2da5f1bfc9
NevenaGatalo <nevena.gatalo@gmail.com>
2021-03-26 21:48:56 +0100

    pocetak
    klasa film i klasa nalog

---
+++ Nevflix/Film.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nevflix
+{
+    enum Zanr { triler, horor, ljubic, akcija, komedija, dokumentarac}
+    class Film
+    {
+        private string naziv;
+        private double rating;
+        private string opis;
+        private List<string> glumci = new List<string>();
+        private int vremeTrajanja;
+        private Zanr zanr ;
+
+
+        public override string ToString()
+        {
+            return $"naziv: {naziv}\n{opis}\nglumci: {glumci.ToString()}\nrating: {rating}\nvreme trajanja: {vremeTrajanja}";
+        }
+        public void PrikaziFilm()
+        {
+            
+        }
+    }
+}
--- Nevflix/Form1.cs
+++ Nevflix/Form1.cs
@@ -19,7 +19,6 @@ namespace Nevflix
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
